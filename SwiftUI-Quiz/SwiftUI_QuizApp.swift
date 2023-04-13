bd0d4f7f284c69d0b328e08d9f479289a4cc2b16
philipmir <philipmir@hotmail.com>
2023-04-13 13:36:05 +0200

    Initial Commit

---
+++ SwiftUI-Quiz/SwiftUI_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_QuizApp.swift
+//  SwiftUI-Quiz
+//
+//  Created by Philip Miranda on 2023-04-13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
